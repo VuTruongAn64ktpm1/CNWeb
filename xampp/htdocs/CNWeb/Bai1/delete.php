7606ad68273876d58cc751f1261a863093422f5a
Truongan <vutruongan15112003@gmail.com>
2024-12-11 01:09:48 +0700

    Initial commit

--- xampp/htdocs/CNWeb/Bai1/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    include 'data.php';
-    $index = $_POST['delete'];
-    array_splice($flowers, $index, 1);
-    file_put_contents('data.php', '<?php $flowers = ' . var_export($flowers, true) . '; ?>');
-    header('Location: admin.php');
-}
-?>
